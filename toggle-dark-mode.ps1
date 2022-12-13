aa5c1918dfcfe3ddddb238ba89f6c28a62460fb5
Ivan Teplov <iteplov7@gmail.com>
2022-12-13 23:50:48 +0200

    Add an option for toggling system theme

--- toggle-dark-mode.ps1
+++ toggle-dark-mode.ps1
@@ -1,8 +1,20 @@
 # Copyright (c) 2022 Ivan Teplov
 
-$registry_item_path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize"
+param (
+    [switch]$ToggleAppTheme= $true,
+    [switch]$ToggleSystemTheme= $false
+)
 
-$is_in_light_mode = (Get-ItemProperty -Path $registry_item_path).AppsUseLightTheme
-$use_light_mode = $is_in_light_mode ? 0 : 1
-Set-ItemProperty -Path $registry_item_path -Name AppsUseLightTheme -Value $use_light_mode
+$registryItemPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize"
 
+if ($ToggleAppTheme) {
+    $isInLightTheme = (Get-ItemProperty -Path $registryItemPath).AppsUseLightTheme
+    $useLightTheme = $isInLightTheme ? 0 : 1
+    Set-ItemProperty -Path $registryItemPath -Name AppsUseLightTheme -Value $useLightTheme
+}
+
+if ($ToggleSystemTheme) {
+    $isInLightTheme = (Get-ItemProperty -Path $registryItemPath).SystemUsesLightTheme
+    $useLightTheme = $isInLightTheme ? 0 : 1
+    Set-ItemProperty -Path $registryItemPath -Name SystemUsesLightTheme -Value $useLightTheme
+}
